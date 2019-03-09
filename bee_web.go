0dc2d56f38d2cba1776b87985c5ae033d40b9a69
ice <tony_mistark@163.com>
2019-03-09 23:02:31 +0800

    end

--- bee_web.go
+++ bee_web.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "github.com/astaxie/beego"
+    "time"
 )
 
 type MainController struct {
@@ -9,10 +10,11 @@ type MainController struct {
 }
 
 func (this *MainController) Get() {
+    time.Sleep(time.Second * 1)
     this.Ctx.WriteString("hello world")
 }
 
 func main() {
-    beego.Router("/hello/world", &MainController{})
+    beego.Router("/hello/world/$", &MainController{})
     beego.Run()
 }
